46686df4416c463049c712fdc34be629d5b9afa0
Brandon Grady <nbhsbg53@gmail.com>
2023-04-03 17:50:42 -0400

    fix todos

---
+++ Develop/client/webpack.config.js
@@ -0,0 +1,84 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    // Entry point for files
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    // Output for our bundles
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+
+    // Add and configure workbox plugins for a service worker and manifest file.
+    plugins: [
+     // Webpack plugin that generates our html file and injects our bundles. 
+     new HtmlWebpackPlugin({
+      // Creates a copy of the index.html file in the dist folder and inserts in script tag to the newly created bundle.js file
+      template: './index.html', 
+      // Optional parameters
+      title: 'Text Editor'
+    }),
+   
+    // Injects our custom service worker
+    new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest: 'src-sw.js',
+    }),
+
+    // Creates a manifest.json file.
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'Just another text editor',
+      short_name: 'JATE',
+      description: 'Just another text editor!',
+      background_color: '#225ca3',
+      theme_color: '#225ca3',
+      start_url: '/',
+      publicPath: '/',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
+    ],
+
+    module: {
+      // Add CSS loaders to webpack
+      rules: [
+        {
+          // This looks for a .css file and adds this into the bundle.js file
+          test: /\.css$/i,
+          // Additional modules for converting the css into js
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // Add babel-loader to webpack in order to use ES6
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/transform-runtime"]
+            }
+          }
+        }
+      ],
+    },
+  };
+};
